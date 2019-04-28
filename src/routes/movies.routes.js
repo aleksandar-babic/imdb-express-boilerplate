87a1ca540ea28b78331c9b8703f371c303c41fdc
Aleksandar Babic <aleksandar.b@vivifyideas.com>
2019-04-28 19:48:04 +0200

    Added dummy movies routes

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const { index } = require('./../services/movies.service');
+const {
+  index, show, destroy, store, update,
+} = require('./../services/movies.service');
 
 router.get('/movies', async (req, res) => res.send(await index()));
+router.get('/movies/:id', async (req, res) => res.send(await show()));
+router.delete('/movies/:id', async (req, res) => res.send(await destroy()));
+router.put('/movies/:id', async (req, res) => res.send(await update()));
+router.post('/movies', async (req, res) => res.send(await store()));
 
 module.exports = router;
